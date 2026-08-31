b60795e8c7e32a64862c645767090be20aec3322
Nicole Fermino Ribeiro <2024105070068@aluno.ifro.edu.br>
2026-08-31 09:20:20 -0400

    feat: desenvolvimento do cadastro de tanques

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tanques = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tanques/cadastrar', methods=["GET", "POST"])
+def cadastrar_tanque():
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+        especie = request.form["especie"]
+        quantidade = request.form["quantidade"]
+
+        tanque = {
+            "id": len(tanques) + 1,
+            "nome": nome,
+            "especie": especie,
+            "quantidade": quantidade
+        }
+
+        tanques.append(tanque)
+
+        return redirect("/tanques")
+
+    return render_template("cadastrar_tanque.html")
+
+@app.route('/tanques')
+def listar_tanques():
+
+    return render_template(
+        "tanques.html",
+    )
